f10a4b95e86e5afa8b52b2ea69946330ea8e3726
arinugraha11 <ari.nugraha@tokopedia.com>
2023-08-20 23:06:56 +0700

    feat: add routing and favicon

---
+++ src/app/auth/components/IconPHR/index.tsx
@@ -0,0 +1,9 @@
+const IconPHR = () => {
+  return (
+    <div>
+      <p>IconPHR</p>
+    </div>
+  );
+};
+
+export default IconPHR;
---
+++ src/app/auth/page.tsx
@@ -0,0 +1,13 @@
+import IconPHR from "./components/IconPHR";
+import LoginForm from "./components/LoginForm";
+
+const Authentication = () => {
+  return (
+    <div>
+      <IconPHR />
+      <LoginForm />
+    </div>
+  );
+};
+
+export default Authentication;
