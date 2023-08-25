3e03bac55afa205f2a551cfb71be07eb8553b9cb
arinugraha11 <ari.nugraha@tokopedia.com>
2023-08-25 08:14:08 +0700

    feat: add vaksinasi anak

--- src/app/vaksinasi/anak/page.tsx
+++ src/app/vaksinasi/anak/page.tsx
@@ -1,5 +1,24 @@
+import Header from "@/app/components/Header";
+import CardPengguna from "../components/CardPengguna";
+import VaksinasiList from "../components/VaksinasiList";
+import Keterangan from "../components/Keterangan";
+
 const VaksinasiAnak = () => {
-  return <div>Vaksinasi anak</div>;
+  return (
+    <div className="flex flex-col">
+      <Header title="Vaksinasi" />
+
+      <div className="flex flex-col w-full px-4 py-4 gap-6">
+        <CardPengguna
+          title="Pilih Pengguna"
+          value="Milo Cornelius (Anak), 12 bulan, laki-laki"
+        />
+
+        <VaksinasiList />
+        <Keterangan />
+      </div>
+    </div>
+  );
 };
 
 export default VaksinasiAnak;
