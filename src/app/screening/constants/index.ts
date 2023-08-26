a83863d33882c07fa166d0e2e3f03ddfcaa00c19
arinugraha11 <ari.nugraha@tokopedia.com>
2023-08-26 15:48:13 +0700

    feat: history screening

--- src/app/screening/constants/index.ts
+++ src/app/screening/constants/index.ts
@@ -12,3 +12,30 @@ export const screeningData = [
     image: "/images/screening/screening-3.png",
   },
 ];
+
+export const dataHistory = [
+  {
+    title: "Tanggal Skrining",
+    data: "18 Maret 2022",
+  },
+  {
+    title: "NIK Pasies",
+    data: "13759027827823",
+  },
+  {
+    title: "Nama Pasien",
+    data: "Jimmy Daniel",
+  },
+  {
+    title: "Jenis Kelamin",
+    data: "Laki-laki",
+  },
+  {
+    title: "Usia",
+    data: "30 tahun",
+  },
+  {
+    title: "Lokasi Skrining",
+    data: "Puskesmas Serpong",
+  },
+];
---
+++ src/app/screening/history/page.tsx
@@ -0,0 +1,24 @@
+import Header from "@/app/components/Header";
+import CardHistory from "../components/HistoryCard";
+import { dataHistory } from "../constants";
+import BottomNavbarMedis from "@/app/components/BottomNavbarMedis";
+
+const ScreeningHistory = () => {
+  return (
+    <div className="flex flex-col">
+      <Header title="Riwayat Skrining Kesehatan" />
+
+      <div className="flex flex-col py-5 px-4">
+        <h3 className="font-semibold text-mainGrey ">Informasi Umum</h3>
+
+        {dataHistory.map((history) => (
+          <CardHistory data={history.data} title={history.title} />
+        ))}
+      </div>
+
+      <BottomNavbarMedis />
+    </div>
+  );
+};
+
+export default ScreeningHistory;
