e2b3f5f87dfa213f8a4ca6c39dc28937512a6eb8
arinugraha11 <ari.nugraha@tokopedia.com>
2023-08-27 00:12:17 +0700

    feat: kirim pesan

---
+++ src/app/pesan/page.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import Header from "../components/Header";
+
+const KirimPesanPage = () => {
+  return (
+    <div className="flex flex-col">
+      <Header title="Kirim Pesan" />
+
+      <div className="flex flex-col w-full px-4 py-4 gap-3">
+        <p className="text-mainGrey text-sm text-justify">
+          Konsultasi atau kirim pesan ke tenaga kesehatan dapat dilakukan dengan
+          aplikasi telekonsultasi di luar aplikasi PHR.
+        </p>
+
+        <div className="flex flex-wrap justify-center">
+          <Image
+            src="/images/pesan/halodoc.png"
+            alt=""
+            width={160}
+            height={160}
+          />
+          <Image
+            src="/images/pesan/alodokter.png"
+            alt=""
+            width={160}
+            height={160}
+          />
+          <Image
+            src="/images/pesan/klikdokter-2.png"
+            alt=""
+            width={160}
+            height={160}
+          />
+          <Image
+            src="/images/pesan/sehatq.png"
+            alt=""
+            width={160}
+            height={160}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default KirimPesanPage;
