982a9d63d824d5952336cd39980653834569cd35
arinugraha11 <ari.nugraha@tokopedia.com>
2023-08-26 23:36:55 +0700

    feat: change background

--- src/app/faskes/components/Chip/index.tsx
+++ src/app/faskes/components/Chip/index.tsx
@@ -4,7 +4,9 @@ interface Props {
 
 const Chip = ({ text }: Props) => {
   return (
-    <p className="bg-lightGrey rounded-2xl px-4 text-sm py-1 mb-1">{text}</p>
+    <p className="bg-superLightGrey rounded-2xl px-4 text-sm py-1 mb-1">
+      {text}
+    </p>
   );
 };
 
