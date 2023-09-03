ec669a3fbadcb1bf238e7e026e9e6a3b00b1aad9
Muhammad Erlangga <muhammad.erlangga@oyindonesia.com>
2023-09-03 23:23:51 +0700

    add news detail

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['down-id.img.susercontent.com']
+    }
+}
 
 module.exports = nextConfig
