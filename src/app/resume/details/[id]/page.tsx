7722b2ed3f8c019189bed6a088e930db84509a60
Muhammad Erlangga <muhammad.erlangga@oyindonesia.com>
2023-09-23 09:17:02 +0700

    finish resume detail

--- src/app/resume/details/[id]/page.tsx
+++ src/app/resume/details/[id]/page.tsx
@@ -2,7 +2,6 @@
 
 import Header from "@/app/components/Header";
 import CardResumeDetail from "../../components/CardResumeDetail";
-import { detailResume } from "../../constants";
 import BottomNavbarMedis from "@/app/components/BottomNavbarMedis";
 import { useParams } from 'next/navigation'
 import { dummyResumeData } from "../../dummyData";
@@ -17,58 +16,94 @@ const ResumeDetail = () => {
     return <div>Not Found</div>
   }
 
+  const renderImage = () => {
+    return (
+      <>
+        <div className="flex-1" style={{
+          backgroundImage: `url("${resume.checkUpImages[0]}")`,
+          backgroundPosition: "center",
+          backgroundSize: "cover"
+        }}>
+        </div>
+        <div className="flex-1" style={{
+          backgroundImage: `url("${resume.checkUpImages[1]}")`,
+          backgroundPosition: "center",
+          backgroundSize: "cover"
+        }}>
+        </div>
+        <div className="flex-1 flex flex-col">
+          <div className="flex-1" style={{
+            backgroundImage: `url("${resume.checkUpImages[2]}")`,
+            backgroundPosition: "center",
+            backgroundSize: "cover"
+          }} />
+          <div className="flex-1" style={{
+            backgroundImage: `url("${resume.checkUpImages[3]}")`,
+            backgroundPosition: "center",
+            backgroundSize: "cover"
+          }} />
+        </div>
+      </>
+    );
+  }
+
   return (
     <div className="flex flex-col">
       <Header title="Resume Medis" />
-      <div className="flex flex-col w-full px-4 py-4 gap-3 mb-16">
+      <div className="flex flex-col w-full px-4 py-4 gap-3 mb-2">
         <h3 className="font-semibold">Informasi Umum</h3>
-          <CardResumeDetail
-            title={"Tanggal Kunjungan"}
-            data={formatDate(resume.date)}
-          />
-          <CardResumeDetail
-            title={"Nomor Rekam Medis"}
-            data={resume.recordNumber}
-          />
-          <CardResumeDetail
-            title={"NIK Pasien"}
-            data={resume.nik}
-          />
-          <CardResumeDetail
-            title={"Jenis Pasien"}
-            data={resume.patientType}
-          />
-          <CardResumeDetail
-            title={"Nama Pasien"}
-            data={resume.patientName}
-          />
-          <CardResumeDetail
-            title={"Nama Tenaga Medis"}
-            data={resume.doctorName}
-          />
-          <CardResumeDetail
-            title={"Nama Poli"}
-            data={resume.polyName}
-          />
-          <CardResumeDetail
-            title={"Nama Fasilitas Kesehatan"}
-            data={resume.location}
-          />
-          <CardResumeDetail
-            title={"Anamnesis"}
-            data={resume.anamnesis}
-          />
-          <CardResumeDetail
-            title={"Diagnosis"}
-            data={resume.diagnose}
-          />
-          <CardResumeDetail
-            title={"Obat yang diberikan"}
-            data={resume.medicine}
-          />
+        <CardResumeDetail
+          title={"Tanggal Kunjungan"}
+          data={formatDate(resume.date)}
+        />
+        <CardResumeDetail
+          title={"Nomor Rekam Medis"}
+          data={resume.recordNumber}
+        />
+        <CardResumeDetail
+          title={"NIK Pasien"}
+          data={resume.nik}
+        />
+        <CardResumeDetail
+          title={"Jenis Pasien"}
+          data={resume.patientType}
+        />
+        <CardResumeDetail
+          title={"Nama Pasien"}
+          data={resume.patientName}
+        />
+        <CardResumeDetail
+          title={"Nama Tenaga Medis"}
+          data={resume.doctorName}
+        />
+        <CardResumeDetail
+          title={"Nama Poli"}
+          data={resume.polyName}
+        />
+        <CardResumeDetail
+          title={"Nama Fasilitas Kesehatan"}
+          data={resume.location}
+        />
+        <CardResumeDetail
+          title={"Anamnesis"}
+          data={resume.anamnesis}
+        />
+        <CardResumeDetail
+          title={"Diagnosis"}
+          data={resume.diagnose}
+        />
+        <CardResumeDetail
+          title={"Obat yang diberikan"}
+          data={resume.medicine}
+        />
 
       </div>
-        {/* TODO: Hasil Pemeriksaan Section */}
+      <div className="flex flex-col w-full px-4 py-4 gap-3 mb-24">
+        <h3 className="font-semibold">Hasil Pemeriksaan</h3>
+        <div className="flex flex-row h-48">
+          {renderImage()}
+        </div>
+      </div>
 
       <BottomNavbarMedis />
     </div>
