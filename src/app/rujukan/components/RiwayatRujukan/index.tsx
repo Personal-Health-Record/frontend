70868b5611d1c99e0e97539593d1fa678ccb2dd2
Muhammad Erlangga <muhammad.erlangga@oyindonesia.com>
2023-09-24 11:29:51 +0700

    finish riwayat rujukan

--- src/app/rujukan/components/RiwayatRujukan/index.tsx
+++ src/app/rujukan/components/RiwayatRujukan/index.tsx
@@ -2,22 +2,18 @@ import { dummyRujukanData } from "../../constants";
 import RiwayatRujukanCard from "../RiwayatRujukanCard";
 
 const RiwayatRujukan = () => {
-  const renderRiwayatRujukanCard = () => {
-    const res: any[] = [];
-
-    let i = 0;
-    while (i < dummyRujukanData.length - 1) {
-      res.push(<RiwayatRujukanCard rujukanData={dummyRujukanData[i]} />)
-    }
-
-    return res;
-  }
-
   return (
     <div className="flex flex-col gap-2 mt-3">
       <h3 className="font-semibold text-mainGrey pl-2">Riwayat Rujukan</h3>
       {
-        ...renderRiwayatRujukanCard()
+        dummyRujukanData.map((rujukanData, index) => {
+          if (index === dummyRujukanData.length - 1) {
+            return;
+          }
+
+          return (
+            <RiwayatRujukanCard rujukanData={rujukanData} key={index} />
+        )})
       }
     </div>
   );
