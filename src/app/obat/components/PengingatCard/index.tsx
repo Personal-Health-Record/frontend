c80f5fd124ba337c3778b9e4d957bc9fe00127b9
Muhammad Erlangga <muhammad.erlangga@oyindonesia.com>
2023-10-09 23:17:55 +0700

    adjust styling

--- src/app/obat/components/PengingatCard/index.tsx
+++ src/app/obat/components/PengingatCard/index.tsx
@@ -10,7 +10,7 @@ const PengingatCard = ({ obat, pengingat: pengigat }: PengingatCardProps) => {
   const renderConsumeButton = () => {
     if (pengigat.consumptionStatus === "Sudah dikonsumsi") {
       return (
-        <div className="flex bg-lightGrey items-center gap-2 pl-3 pr-6 py-1 rounded-2xl">
+        <div className="flex bg-lightGrey items-center gap-1 pl-1 pr-2 py-1 rounded-2xl">
           <Image
             src="/images/pengingat/check_all.png"
             alt=""
@@ -23,7 +23,7 @@ const PengingatCard = ({ obat, pengingat: pengigat }: PengingatCardProps) => {
     }
 
     return (
-      <div className="flex bg-mainBlue items-center gap-2 px-3 py-1 rounded-2xl">
+      <div className="flex bg-mainBlue items-center gap-2 px-2 py-1 rounded-2xl">
         <p className="text-xs text-white">Konsumsi</p>
       </div>
     )
@@ -31,13 +31,12 @@ const PengingatCard = ({ obat, pengingat: pengigat }: PengingatCardProps) => {
 
   return (
     <div className="flex shadow-md py-3 px-2 rounded-xl gap-3 justify-between items-center">
-      <div>
+      <div className="flex justify-start items-center">
         <Image src="/images/pengingat/obat.png" alt="" width={50} height={50} />
-      </div>
-
-      <div className="flex flex-col justify-between">
-        <p className="text-xs">{obat.name}</p>
-        <p className="text-xs text-lightGrey">{obat.consumptionPerDay} pil per hari, {obat.consumptionTime}, {obat.consumptionMethod}</p>
+        <div className="flex flex-col justify-between ml-3">
+          <p className="text-xs">{obat.name}</p>
+          <p className="text-xs text-lightGrey">{obat.consumptionPerDay} pil per hari, {obat.consumptionTime}, {obat.consumptionMethod}</p>
+        </div>
       </div>
 
       <div className="flex flex-col items-end gap-1 mr-3">
