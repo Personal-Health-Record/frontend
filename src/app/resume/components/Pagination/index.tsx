86aa07bdeeee998f5e64bb73497c80fb23428168
arinugraha11 <ari.nugraha@tokopedia.com>
2023-10-22 21:26:36 +0700

    feat: fix pagination

--- src/app/resume/components/Pagination/index.tsx
+++ src/app/resume/components/Pagination/index.tsx
@@ -27,7 +27,11 @@ const Pagination = (props: PaginationProps) => {
     }
     return pageNumbers.map((number) => (
       <p
-        className="text-xs"
+        className={`text-xs ${
+          number === pageNumber
+            ? 'bg-mainBlue px-3 py-1 rounded-2xl'
+            : 'text-mainGrey'
+        }`}
         key={number}
         onClick={() => {
           setPageNumber(number);
@@ -39,7 +43,7 @@ const Pagination = (props: PaginationProps) => {
   };
 
   return (
-    <div className="flex justify-between items-center mb-32">
+    <div className="flex justify-between items-center mb-32 px-4">
       <button
         className="bg-superLightGrey py-1 px-2 rounded-xl text-xs text-mainGrey"
         onClick={handlePrevPage}
